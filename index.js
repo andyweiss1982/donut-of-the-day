adc6164bd6aeb630f4d15b7256d5fbc3cee6a1e8
Andy Weiss <andyweiss1982@gmail.com>
2020-05-27 12:55:58 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const PORT = process.env.PORT || 3000;
+
+pool.query(`
+  CREATE TABLE IF NOT EXISTS votes (
+    id SERIAL PRIMARY KEY,
+    donut VARCHAR(256) NOT NULL,
+    voter VARCHAR(256) NOT NULL,
+    date DATE DEFAULT CURRENT_DATE
+  );
+  CREATE UNIQUE INDEX IF NOT EXISTS votes_voter_date on votes (voter, date);
+`);
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/votes", async (req, res) => {
+  const { voter, donut } = req.body;
+  await pool.query(
+    `DELETE FROM votes where voter = $1 AND date = CURRENT_DATE;`,
+    [voter]
+  );
+  await pool.query(
+    `INSERT INTO votes (voter, donut, date) VALUES ($1, $2, CURRENT_DATE);`,
+    [voter, donut]
+  );
+  res.sendStatus(200);
+});
+
+app.get("/votes", async (req, res) => {
+  const { rows } = await pool.query(
+    `SELECT * FROM votes WHERE date = CURRENT_DATE;`
+  );
+  const votes = rows.reduce((object, row) => {
+    object[row.donut] = object[row.donut] ? object[row.donut]++ : 1;
+    return object;
+  }, {});
+  res.json(votes);
+});
+
+app.listen(PORT, console.log(`Server listening on port ${PORT} 🚀`));
---
+++ public/donuts.js
@@ -0,0 +1,81 @@
+if (!localStorage.getItem("voterId")) {
+  localStorage.setItem("voterId", String(Math.random()));
+}
+
+const voterId = localStorage.getItem("voterId");
+
+const query = `
+  query {
+    Section(id: "section_7470"){
+      options{
+        ... on Item {
+          _id
+          name{
+            en
+          }
+          image{
+            asset{
+              url
+            }
+          }
+        }
+      }
+    }
+  }`;
+
+const renderDonut = (donut) => `
+  <section class="donut" id="${donut._id}">
+    <img src="${donut.image?.asset?.url}" alt="${donut.name.en}"/>
+    <h2 class="vote-count">0</h2>
+    <button onclick="vote('${donut._id}')">${donut.name.en}</button>
+  </section>`;
+
+const fetchDonuts = async () => {
+  const response = await fetch(
+    `https://czqk28jt.api.sanity.io/v1/graphql/prod_th/default`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query }),
+    }
+  );
+  const {
+    data: {
+      Section: { options },
+    },
+  } = await response.json();
+  const displayDonuts = options.filter(
+    (option) => option.name?.en && option.image?.asset?.url
+  );
+  main.innerHTML = displayDonuts.map((donut) => renderDonut(donut)).join("");
+  fetchVotes();
+};
+
+const vote = async (donutId) => {
+  localStorage.setItem("selectedDonutId", donutId);
+  await fetch("/votes", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ donut: donutId, voter: voterId }),
+  });
+  fetchVotes();
+};
+
+const fetchVotes = async () => {
+  const response = await fetch("/votes");
+  const votes = await response.json();
+  const donuts = [...document.querySelectorAll(".donut")];
+  const selectedDonutId = localStorage.getItem("selectedDonutId");
+  donuts.forEach((donut) => {
+    donut.querySelector(".vote-count").textContent = votes[donut.id] || 0;
+    if (donut.id !== selectedDonutId) {
+      donut.classList.remove("selected");
+    }
+  });
+  if (selectedDonutId) {
+    document.getElementById(selectedDonutId).classList.add("selected");
+  }
+};
+
+fetchDonuts();
+setInterval(fetchVotes, 1000);
